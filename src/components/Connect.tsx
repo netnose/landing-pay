b0bd9f0cbb5c86c4dd14fcb9a8e15ef7385d2d60
Mirko Nosenzo <netnose@gmail.com>
2024-06-08 00:42:40 +0200

    Chain Set on Connection

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -1,6 +1,6 @@
-import { Avatar, Name } from "@coinbase/onchainkit/identity";
-import { Dispatch, SetStateAction, useEffect } from "react";
-import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { Dispatch, SetStateAction } from "react";
+import { base } from "viem/chains";
+import { useConnect } from "wagmi";
 
 export function Connect({
   setStep
@@ -13,7 +13,7 @@ export function Connect({
   return <section className="connect">
     <h1 className="connect-title">Connect a wallet</h1>
     {connectors.map((connector) => (
-      <button key={connector.id} type="button" onClick={() => connect({ connector })}>{connector.name}</button>
+      <button key={connector.id} type="button" onClick={() => connect({ chainId: base.id, connector })}>{connector.name}</button>
     ))}
     { coinbaseWalletConnector && coinbaseWalletConnector[0] && <button className="secondary" type="button" onClick={() => connect({ connector: coinbaseWalletConnector[0] })}><span className="soft">Don&apos;t have a wallet?</span> Create a Smart Wallet</button>}
   </section>;
